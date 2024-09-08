f9210538f4dfc0fc68823576dc46afa4a505db8e
Hyun-h <hyeonahong@gmail.com>
2024-09-08 14:38:56 +0900

    fix: 사진 순서 변경 및 설명 추가

--- src/style/layout/Gallery/index.tsx
+++ src/style/layout/Gallery/index.tsx
@@ -6,6 +6,10 @@ const Gallery = () => {
   return (
     <ItemSection>
       <Typography variant="h2">사진첩</Typography>
+      <Typography mb={2} fontWeight={300}>
+        신부 가족의 추억이 가득한 경남 사천과 진주에서 촬영하였어요. 참고로
+        식장은 신랑이 가족들과 추억이 가득한 장소에요.
+      </Typography>
       <ImageSwiper />
     </ItemSection>
   );
