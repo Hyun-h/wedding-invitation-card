db21a521c6a284c58e6a44b635ec681626a1aa63
Hyun-h <hyeonahong@gmail.com>
2024-09-08 15:06:42 +0900

    fix: 갤러리 설명 문구 수정

--- src/style/layout/Gallery/index.tsx
+++ src/style/layout/Gallery/index.tsx
@@ -7,8 +7,8 @@ const Gallery = () => {
     <ItemSection>
       <Typography variant="h2">사진첩</Typography>
       <Typography mb={2} fontWeight={300}>
-        신부 가족의 추억이 가득한 경남 사천과 진주에서 촬영하였어요. 참고로
-        식장은 신랑이 가족들과 추억이 가득한 장소에요.
+        신부와 신부 가족의 추억이 가득한 경남 사천과 진주에서 촬영하였어요.
+        참고로 결혼하는 곳은 신랑이 결혼 전까지 가족들과 기억을 빚어온 장소예요.
       </Typography>
       <ImageSwiper />
     </ItemSection>
